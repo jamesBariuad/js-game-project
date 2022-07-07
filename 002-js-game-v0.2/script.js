93920c84bbaf9b029dfbbde54f3fb54e536d6d46
james bariuad <jamesbariuad@gmail.com>
2022-07-08 00:45:17 +0800

    002-js-game v0.2

--- 002-js-game-v0.2/script.js
+++ 002-js-game-v0.2/script.js
@@ -1,16 +1,10 @@
-// make a memorization game repeating colors
 
-//create 1 main div
-//inside it create 9 squares using loop
-
-//create modal on how to play
 const titleHowToPlay = document.querySelector(".titleHowToPlay");
 const overlay = document.querySelector(".overlay");
 const howToPlayBtn = document.querySelector(".howToPlayBtn");
 const menu = document.querySelector(".menu");
 
-
-
+//showing and removing instructions on how to play
 titleHowToPlay.addEventListener("click",close);
 howToPlayBtn.addEventListener("click", open);
 
@@ -25,28 +19,28 @@ function open(){
 }
 
 
-//create event listener on start game
+//create event listener to start game
 const startBtn = document.querySelector(".startBtn");
 startBtn.addEventListener("click", startGame);
 
 function startGame(){
     startBtn.remove(startBtn);
     howToPlayBtn.remove(howToPlayBtn);
-    const menu = document.querySelector(".menu")
-    menu.textContent = "Repeat after me!"
-    menu.textalign = "center"
-    return counterOfLights()
-}
+    menu.textContent = "Repeat after me!";
+    menu.textalign = "center";
+    return counterOfLights();
+};
 
-
-//make a random box picker
+//an array that stores the pattern of answers
 let pattern = [];
+
+//addition of classes square1 square2... to DOM array
 let areasDomArray = ["square1", "square2", "square3", "square4", "square5", "square6", "square7", "square8"];
-//addition of classes to DOM array
 for (i=0; i<=7; i++) {
     areasDomArray[i] = document.querySelector(".square"+(i+1));
 }
 
+//initialization
 let counter = 2;
 function counterOfLights(){
     setTimeout(function() {
@@ -59,6 +53,7 @@ function counterOfLights(){
     }, 1000);
 }
 
+//randomizer of what number of square to be put in pattern array
 function randomizer(){
     let number = Math.floor((Math.random() * 8) +1);
     pattern.push(number)
@@ -70,7 +65,9 @@ function randomizer(){
     }
 }
 
+//makes the generated pattern light up
 function light(){
+    playAudio();
     for (let i = 0; i < pattern.length; i++) {
         setTimeout(function() {
             areasDomArray[pattern[i]-1].classList.add("glow");
@@ -87,45 +84,79 @@ function light(){
     }, 1000*pattern.length);
 }
 
-
-
-patternIndex = 0;
+//makes the clicks light up on different divs, checks if the pattern of clicks is the same on the generated pattern
+let patternInput = 0;
+let score = 0;
+let hiScore = 0;
 function lightOnClick(){
-    
-    // console.log(this.classList.value)
     let z = this.classList.value;
     
+    let audio = new Audio("audio/"+z[6]+".wav")
+    audio.play()
+
     areasDomArray[z[6]-1].classList.add("glow")
     setTimeout(function() {
-        // this.classList.remove("glow");
-      
         areasDomArray[z[6]-1].classList.remove("glow")
     }, 500);
+
+
     
-    if (z[6]!=pattern[patternIndex]) {
+    if (z[6]!=pattern[patternInput]) {
+        if (score<3){
+            score=score;
+            if (hiScore<score){
+                hiScore=score
+            }
+        }
+        else{
+            score=pattern.length-1;
+            if (hiScore<score){
+                hiScore=score
+            }
+        }
         for (i=0; i<areasDomArray.length; i++){
             areasDomArray[i].removeEventListener("click", lightOnClick);
         }
-        menu.textContent = "you lose"
+        
+        menu.innerHTML = "You Lose<br>Score: "+score+"<br> Hi-score:"+hiScore;
         restartBtn = document.createElement("button");
-        restartBtn.textContent = "restart?"
+        restartBtn.textContent = "Restart?"
         menu.appendChild(restartBtn)
+        menu.appendChild(howToPlayBtn);
         restartBtn.addEventListener("click", resetAll)
+        return score;
     }
-    else if (patternIndex+1==pattern.length) {
-        patternIndex = 0;
-         return counterOfLights()
+    else if (patternInput+1==pattern.length) {
+        patternInput = 0;
+        return counterOfLights()
     }  
-    patternIndex++
+    patternInput++
+    score++
 }
 
+//resets variables on new game
 function resetAll(){
-    pattern = []
-    counter = 2
-    patternIndex = 0;
-    restartBtn.remove(restartBtn)
-    return startGame()
+    pattern = [];
+    counter = 2;
+    patternInput = 0;
+    restartBtn.remove(restartBtn);
+    score = 0;
+    return startGame();
 }
 
+function playAudio(){
+    for (let i = 0; i < pattern.length; i++) {
+        setTimeout(function() {
+            let audio = new Audio("audio/"+(pattern[i])+".wav")
+             audio.play()
+        }, i * 1000);
+    }
+}   
 
-// function yourTurn()
+// function musicIsLyf(){
+//     for (i=0; i<areasDomArray.length; i++){
+//         areasDomArray[i].addEventListener("click", lightOnClick);
+//     }  
+//     let audio = new Audio("audio/"+z[6]+".wav")
+//     audio.play()
+// }
